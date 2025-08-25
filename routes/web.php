749cc10272d57d89dc67265a1c68249c5c17df7b
Gabriel Patano Kiyot <gabrielkiyoto@unifev.corp.br>
2025-08-25 15:34:11 -0300

    rotas de visualização

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-
-Route::redirect('rota-a', 'rota-b', 301);
-Route::permanentRedirect('rota-a', 'rota-b');
-
-Route::get('rota-b', function() {
-    return 'Rota B';
-});
+Route::view('/', 'welcome', ['title' => 'Hello World']);
