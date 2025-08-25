7d04f918a89d2502f6e868fbca9bfb6b7c9f39a5
Gabriel Patano Kiyot <gabrielkiyoto@unifev.corp.br>
2025-08-25 16:01:30 -0300

    validando parâmetros

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('token/{token}', function($token) {
+    return $token;
+})->whereNumber('token');
+
 Route::get('/user/{id?}/{name?}', function($id = null, $name = null) {
     return 'User ' . $id . ' - ' . $name;
-});
+})->where([
+    'id' => '[0-9]+',
+    'name' => '[A-Za-z]+'
+]);
 
 Route::get('/', function() {
     return view('welcome');
